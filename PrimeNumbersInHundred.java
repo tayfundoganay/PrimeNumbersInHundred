22f832dc6a245b0d3ff79accac9fae61717c31ad
Tayfun DOGANAY <73904292+tayfundoganay@users.noreply.github.com>
2022-11-12 23:45:32 +0300

    Create PrimeNumbersInHundred.java

---
+++ PrimeNumbersInHundred.java
@@ -0,0 +1,16 @@
+public class PrimeNumbersInHundred {
+
+    public static void main(String[] args) {
+
+        for (int i = 2; i <= 100; i++) {
+            boolean isPrime = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                }
+            }
+            if (isPrime)
+                System.out.print(i + ", ");
+        }
+    }
+}
